08fcc55c2696ea01d0f90bc607bc3bced65414e8
johker <johannes.kern@zoho.com>
2021-04-02 23:07:24 +0200

    Subscribed to multiple topics

--- src/main.rs
+++ src/main.rs
@@ -30,25 +30,35 @@ fn main() {
         data: vec![0; DEF_PL_SIZE + PAYLOAD_OFFSET],
     };
 
-    m.create_header(
-        MessageType::DATA,
-        MessageCommand::INPUT,
-        MessageKey::S_SPOOL,
-    );
-
     // Initialize publisher
     let publisher = context.socket(zmq::PUB).unwrap();
     assert!(publisher.connect("tcp://localhost:6000").is_ok());
 
     // Initialize subsciber
-    let sub_topic = m.get_topic();
     thread::spawn(move || {
         let subscriber = context.socket(zmq::SUB).unwrap();
         assert!(subscriber.connect("tcp://localhost:5555").is_ok());
         subscriber
-            .set_subscribe(&sub_topic.as_bytes())
+            .set_subscribe(
+                &format!(
+                    "T{:03}.{:03}",
+                    MessageType::CONFIGURATION as u16,
+                    MessageCommand::WRITE as u16
+                )
+                .as_bytes(),
+            )
             .expect("Failed to subscribe");
-        println!("Subscribed to {:?}", sub_topic.as_bytes());
+
+        subscriber
+            .set_subscribe(
+                &format!(
+                    "T{:03}.{:03}",
+                    MessageType::DATA as u16,
+                    MessageCommand::WRITE as u16
+                )
+                .as_bytes(),
+            )
+            .expect("Failed to subscribe");
 
         loop {
             let s = subscriber.recv_bytes(0).unwrap();
@@ -75,20 +85,18 @@ fn main() {
 
     for received in rx {
         if received.len() > 1 {
-            println!("RAW: {:?}", received);
+            // println!("RAW: {:?}", received);
             if received[0] == 84 {
-                // Topic
+                // Starts with T => Topic
                 continue;
             }
             m.data = received;
-            println!("RECV {}", m.print());
+            println!("RECV MSG (TOPIC: {})", m.get_topic());
             if let Some(MessageCommand::INPUT) = m.get_cmd() {
                 m.parse_to(&mut input);
 
                 // Compute next acitvation
-                println!("Computing update ...");
                 sp.compute(&input, true);
-                println!("Done.");
 
                 m.create_header(
                     MessageType::DATA,
@@ -102,9 +110,8 @@ fn main() {
                     m.set_payload_bit(col_idx);
                 }
 
-                let pub_topic = MessageCommand::PRINT;
-                println!("TOPIC {}", m.get_topic());
-                println!("SENT {}", m.print());
+                println!("SENT MSG (TOPIC: {})", m.get_topic());
+                // println!("SENT {}", m.print());
                 publisher.send(&m.get_topic(), zmq::SNDMORE).unwrap();
                 publisher.send(&m.data, 0).unwrap();
             }
